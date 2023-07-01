69aa82dc958153743f451c91732ece0ee6ba6578
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-02 00:56:24 +0200

    Added function to get needed display elements

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import './style.css';
 
-console.log("Hello World!");
+function init() {
+  const numberToConvertEl = document.getElementById('convert') as HTMLSpanElement | null;
+  const currentBaseEl = document.getElementById('base-1') as HTMLSpanElement | null;
+  const wantedBaseEl = document.getElementById('base-2') as HTMLSpanElement | null;
+
+  if (!numberToConvertEl) throw new Error('No instance of element with id "convert"');
+  else if (!currentBaseEl) throw new Error('No instance of element with id "base-1"');
+  else if (!wantedBaseEl) throw new Error('No instance of element with id "base-2"');
+
+  return { numberToConvertEl, currentBaseEl, wantedBaseEl };
+}
+
+console.log(init());
