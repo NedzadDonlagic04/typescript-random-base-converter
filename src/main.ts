d3fbfc974437f19894349b7e2d948cee210fa9f6
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-02 16:01:40 +0200

    Added randomly generated numbers on page load

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import './style.css';
+import functions from './functions';
+import constants from './constants';
+
+let bases: number[] = [...constants.BASES];
 
 function init() {
   const numberToConvertEl = document.getElementById('convert') as HTMLSpanElement | null;
-  const currentBaseEl = document.getElementById('base-1') as HTMLSpanElement | null;
-  const wantedBaseEl = document.getElementById('base-2') as HTMLSpanElement | null;
+  const convertingFromBaseEl = document.getElementById('base-1') as HTMLSpanElement | null;
+  const convertingToBaseEl = document.getElementById('base-2') as HTMLSpanElement | null;
 
   if (!numberToConvertEl) throw new Error('No instance of element with id "convert"');
-  else if (!currentBaseEl) throw new Error('No instance of element with id "base-1"');
-  else if (!wantedBaseEl) throw new Error('No instance of element with id "base-2"');
+  else if (!convertingFromBaseEl) throw new Error('No instance of element with id "base-1"');
+  else if (!convertingToBaseEl) throw new Error('No instance of element with id "base-2"');
 
-  return { numberToConvertEl, currentBaseEl, wantedBaseEl };
+  return { numberToConvertEl, convertingFromBaseEl, convertingToBaseEl };
 }
 
-console.log(init());
+const { numberToConvertEl, convertingFromBaseEl, convertingToBaseEl } = init();
+
+
+const [numberToConvert, convertingFromBase, convertToBase] = functions.randomizeNumbers(numberToConvertEl, convertingFromBaseEl, convertingToBaseEl, bases);
+
+console.log(numberToConvert, convertingFromBase, convertToBase);
